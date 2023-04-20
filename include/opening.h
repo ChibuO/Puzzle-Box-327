30273be39f956827051959add1d1d2e19b9d3809
hare-care <hbajramovic27@gmail.com>
2023-04-20 17:41:39 -0500

    added header for opening

---
+++ include/opening.h
@@ -0,0 +1,10 @@
+#ifndef OPENING_H
+#define OPENING_H
+    #include <Arduino.h>
+    #include <ESP32Servo.h>
+    #include <cmath>
+
+    void open_setup();
+    void open();
+#endif /* OPENING_H */
+

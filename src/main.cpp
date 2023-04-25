47379efc0ee16c256bcc705652ad3904edd8c206
hare-care <hbajramovic27@gmail.com>
2023-04-25 14:26:31 -0500

    added light puzzle from seperated file

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,28 @@
 #include <Arduino.h>
 #include "opening.h"
 #include "password.h"
+#include "light_knobs.h"
 
-const byte POT_PIN1 = 36;
-const byte POT_PIN2 = 39;
-const byte POT_PIN3 = 34;
-const byte POT_PIN4 = 35;
+
 const byte led_gpio = 32;
 
-#define midnight 1860
-#define gloaming 2320
-#define dawn 2480
-#define noon 3800
-
-int sequence[] = {gloaming, midnight, dawn, noon};
-int led_status[] = {midnight, midnight, midnight, midnight};
 
 void setup() {
   Serial.begin(9600);
   pinMode(led_gpio, OUTPUT);
   keypad_setup();
-}
-
-int get_status(uint16_t ar) {
-  if (ar < midnight) {
-    return midnight;
-  } else if (ar > midnight && ar < gloaming) {
-    return gloaming;
-  } else if (ar > gloaming && ar < dawn) {
-    return dawn;
-  } else {
-    return noon;
-  }
+  light_knobs_setup();
 }
 
 void loop() {
-  led_status[0] = get_status(analogRead(POT_PIN1));
-  led_status[1] = get_status(analogRead(POT_PIN2));
-  led_status[2] = get_status(analogRead(POT_PIN3));
-  led_status[3] = get_status(analogRead(POT_PIN4));
+  update_led_status();
 
-  // led_status[0] = analogRead(POT_PIN1);
-  // led_status[1] = analogRead(POT_PIN2);
-  // led_status[2] = analogRead(POT_PIN3);
-  // led_status[3] = analogRead(POT_PIN4);
-
-  bool is_correct = led_status[0] == sequence[0] && led_status[1] == sequence[1] && led_status[2] == sequence[2] && led_status[3] == sequence[3];
-
-  if (is_correct) {
+  if (led_is_correct()) {
     digitalWrite(led_gpio, HIGH);   // turn the LED on (HIGH is the voltage level)
   } else {
     digitalWrite(led_gpio, LOW);    // turn the LED off by making the voltage LOW
   }
-
-  String out_str = String(led_status[0]) + " " + String(led_status[1]) + " " + String(led_status[2]) + " " + String(led_status[3]);
-  Serial.println(out_str);
+  print_led_status();
   delay(300);
 
   /*

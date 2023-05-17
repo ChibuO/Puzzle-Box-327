915c2bbc6b8307ca211ae279cd7e8632875b7de1
Chibu O <56417244+ChibuO@users.noreply.github.com>
2023-05-16 22:23:19 -0500

    added neopixels

---
+++ include/neos.h
@@ -0,0 +1,33 @@
+#ifndef NEOS_H_
+#define NEOS_H_
+    #include <Adafruit_NeoPixel.h>
+    #ifdef __AVR__
+        #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+    #endif
+    
+    // Which pin on the Arduino is connected to the NeoPixels?
+    // On a Trinket or Gemma we suggest changing this to 1:
+    #define NEOS_LED_PIN    12
+
+    // How many NeoPixels are attached to the Arduino?
+    #define LED_COUNT 8
+
+    // Declare our NeoPixel strip object:
+    // Adafruit_NeoPixel strip(LED_COUNT, NEOS_LED_PIN, NEO_GRB + NEO_KHZ800);
+    // Argument 1 = Number of pixels in NeoPixel strip
+    // Argument 2 = Arduino pin number (most are valid)
+    // Argument 3 = Pixel type flags, add together as needed:
+    //   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+    //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+    //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+    //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+    //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+
+    void neopixel_setup();
+    uint32_t rgb_to_binary(uint8_t r, uint8_t g, uint8_t b);
+    void colorWipe(uint32_t color, int wait);
+    void theaterChase(Adafruit_NeoPixel strip, uint32_t color, int wait);
+    void rainbow(Adafruit_NeoPixel strip, int wait);
+    void theaterChaseRainbow(Adafruit_NeoPixel strip, int wait);
+
+#endif /* NEOS_H */
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,8 @@
-#include <Arduino.h>
-#include "opening.h"
-#include "password.h"
-#include "light_knobs.h"
-#include "server.h"
-#include "filesystem.h"
-#include "imu.h"
+#include "main.h"
 
 const byte led_gpio = 32;
 const byte led_gpio2 = 33;
-bool lights_done = 1;
+bool lights_done = 0;
 bool keypad_done = 0;
 bool door_open = 0;
 int num_completed = 0;
@@ -22,9 +16,10 @@ void setup()
   pinMode(led_gpio, OUTPUT);
   pinMode(led_gpio2, OUTPUT);
   keypad_setup();
-  //light_knobs_setup();
+  light_knobs_setup();
   open_setup();
   imu_setup();
+  neopixel_setup();
 
   // Initialize SPIFFS - for saving data in flash memory
   uint8_t spiffs_check = startSPIFFS();
@@ -47,6 +42,21 @@ void send_to_socket(String data) {
   ws.broadcastTXT(json);
 }
 
+void puzzle_complete() {
+  num_completed++;
+  colorWipe(rgb_to_binary(  0, 255,   0), 100); // Green
+  colorWipe(rgb_to_binary(  0, 0,   0), 50); // dark
+  // delay(500);
+  colorWipe(rgb_to_binary(  0, 255,   0), 100); // Green
+  colorWipe(rgb_to_binary(  0, 0,   0), 50); // dark
+  // delay(500);
+  colorWipe(rgb_to_binary(  0, 255,   0), 100); // Green
+  // delay(1500);
+  colorWipe(rgb_to_binary(  0, 0,   0), 50); // dark
+  send_to_socket("");
+}
+
+
 void puzzle() {
   // lights puzzle
   while (!lights_done) {
@@ -63,25 +73,23 @@ void puzzle() {
     delay(300);
   }
 
-  num_completed++;
+  puzzle_complete();
+  
   
   //weight
-  num_completed++;
-
-  send_to_socket("");
+  puzzle_complete();
 
   // code for keypad
   while (!keypad_done) {
     keypad_done = keypad_check_password(6, password);
     digitalWrite(led_gpio2, HIGH);
-    send_to_socket("");
   }
 
   Serial.write("******************************\r\n");
   Serial.write("YOU WIN\r\n");
   Serial.write("******************************\r\n");
 
-  num_completed++;
+  puzzle_complete();
 
   while(1) {
     send_to_socket("");
@@ -103,12 +111,13 @@ void loop()
     // }
   } else {
     Serial.println("buzzzzzzzzzzzzz");
-    //if (!door_open)
     num_completed++;
     while (!open()) {
-      send_to_socket("");
     };
+    send_to_socket("");
+
     puzzle();
+    // puzzle_complete();
   }
   
 
